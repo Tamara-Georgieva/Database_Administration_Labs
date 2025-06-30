dc145bb78942d6138fbaeb23fa56d89a8b4b009b
Tamara-Georgieva <tamarageorgieva2001@gmail.com>
2025-06-30 12:51:40 +0200

    Update Medical_DataBase.sql

--- Practice/Medical_DataBase.sql
+++ Practice/Medical_DataBase.sql
@@ -8,3 +8,12 @@ Vakcinacija(id_lice*, id_med_lice*, shifra_vakcina*)
 Vakcinacija_datum(id_lice*, id_med_lice*, shifra_vakcina*, datum)
 
 Да се напише DML израз со кој ќе се вратат матичните броеви на лицата (сортирани во растечки редослед) кои биле позитивни и потоа примиле барем една доза вакцина.
+SELECT DISTINCT l.id
+FROM Lice l
+JOIN Test t ON l.id = t.id
+JOIN Vakcinacija_datum vd ON l.id = vd.id_lice
+WHERE t.rezultat = 'pozitiven'
+AND t.datum < vd.datum
+GROUP BY l.id
+HAVING COUNT(vd.datum) >= 1
+ORDER BY l.id ASC;
